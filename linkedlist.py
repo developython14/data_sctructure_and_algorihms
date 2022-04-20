6aaaf845114667f345f2d08b890b754f56cb88f8
developython14 <djmel1955@gmail.com>
2022-04-20 19:05:30 +0100

    work on construction of data

--- linkedlist.py
+++ linkedlist.py
@@ -20,5 +20,30 @@ class linkedlist :
         """_initilaite the stack_
         """
         self.head = None
-        self._size = 0 
+        self._size = 0
+
+    def __len__(self):
+        """_return the lenght of the stack_
+        """
+        return self._size
+    
+    def is_empty(self):
+        """_test if the stack is empty_
+
+        Returns:
+            _type_: boolean
+        """
+        return self._size == 0
+    
+    def push(self,e):
+        """_push an element to the list_
+
+        Args:
+            e (_type_): _a new element to the stack _
+        """
+        self.head = self._node(e,self.head)
+        self._size += 1
+    def top(self):
+        """return the top of the stack """
+            
     
