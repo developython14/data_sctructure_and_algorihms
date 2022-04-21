c7944cf9bca500ea6887f26f3f4d48a1f7269306
developython14 <djmel1955@gmail.com>
2022-04-21 11:34:34 +0100

    work on queue using linkedlist

--- queue.py
+++ queue.py
@@ -14,4 +14,37 @@ class queue :
             self.tail = None
             self._size = 0 
         
+
+    def __len__(self):
+        """_return the lenght of the stack_
+        """
+        return self._size
+    
+    def is_empty(self):
+        """_test if the stack is empty_
+
+        Returns:
+            _type_: boolean
+        """
+        return self._size == 0
+
+    def top(self):
+        """_public funtion that return the top of stack or the next elemenent in ordering_
+        """
+
+
+        return
+    
+    def push(self,e):
+        """_public function that append new element in the stack_
+
+        Args:
+            e (_object_): _element that we will put it in the stack_
+        """
+
+
+    def pop(self):
+        """_public function that return the top element of the stack and remove it_
+        """
+
         
